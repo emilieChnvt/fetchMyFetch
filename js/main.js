660b777480805b61b6a93519b110bdb625be18f8
Emilie <emilie.chnvt@gmail.com>
2024-11-22 14:21:29 +0100

    POST method added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+let leBodyEnJSON = JSON.stringify({
+    title:'foo',
+    body:'bar',
+    userId: 1
+})
 
+let lesHeaders={
+    'Content-type':'application/json',
+}
 
 // GET
 
@@ -6,5 +14,15 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.json())
     .then(data => console.log(data))
 
+
 //POSTS
- .then(data => console.log(data))
+
+fetch('https://jsonplaceholder.typicode.com/posts/',{
+    method: 'POST',
+    headers: lesHeaders,
+    body: leBodyEnJSON
+
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+
